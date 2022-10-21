94cd71e2ca058147d0ecd8d291d6d4f0d289bce8
AlekseyMegliy <meglij46@gmail.com>
2022-10-21 22:24:12 +0300

    Tried to replace the header using PHP (failed)

---
+++ header.php
@@ -0,0 +1,41 @@
+<header class="container">
+    <div class="row">
+        <a href="./index.html" class="col-5 col-sm-3 col-lg-2 offset-1 align-items-center logo">
+            <img width="165px"  src="img/logo.png">
+        </a>
+        <button class="offset-6 header-button">
+            <svg>
+                <rect x="10" y="6" width="15" height="2"/>
+                <rect x="10" y="11" width="15" height="2"/>
+                <rect x="10" y="16" width="15" height="2"/>
+            </svg>
+        </button>
+        <div class="col-sm-11 offset-sm-1 col-lg-5 col-xl-5 offset-lg-2 offset-xl-3 nav">
+            <a href="./index.html" class="current">
+                <img src="ico//home-sm.png">
+                <p>HOME</p>
+            </a>
+            <a href="#">
+                <img src="ico/portfolio-sm.png">
+                <p>PORTFOLIO</p>
+            </a>
+            <a href="#">
+                <img src="ico/blog-sm.png">
+                <p>BLOG</p>
+            </a>
+            <a href="#">
+                <img src="ico/services-sm.png">
+                <p>SERVICES</p>
+            </a>
+            <a href="#">
+                <img src="ico/about-sm.png">
+                <p>ABOUT</p>
+            </a>
+            <a href="#">
+                <img src="ico/contact-sm.png">
+                <p>CONTACT</p>
+            </a>
+        </div>
+        <span class="col-1"></span>
+    </div>
+</header>
