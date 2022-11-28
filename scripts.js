61e3bfbbda929636a709432595cb288ad1d1318c
AlekseyMegliy <meglij46@gmail.com>
2022-11-28 14:39:57 +0200

    made small_img and testimonials blocs

--- scripts.js
+++ scripts.js
@@ -4,53 +4,44 @@ let slider_button_right = document.querySelector(".slider-button-right");
 let slides = document.querySelectorAll(".slide");
 let small_imgs = document.querySelectorAll(".sm-img");
 let testimonial = document.querySelectorAll(".testimonial");
+let testimonial_buttons = document.querySelectorAll(".buttons");
+let benefits = document.querySelectorAll(".benefits div p");
 let slide_mass = [];
 let small_imgs_mass = [];
 let testi_mass = [];
+let testi_butt_mass = [];
+let benef_mass = [];
 let count = 0;
 let testi_count =0;
+
+for(let slide of slides){
+    slide_mass.push(slide);
+}
+for(let small_img of small_imgs){
+    small_imgs_mass.push(small_img);
+}
+for(let testi of testimonial){
+    testi_mass.push(testi);
+}
+for(let testi_butt of testimonial_buttons){
+    testi_butt_mass.push(testi_butt);
+}
+
+
 
 function headerClick(){
     header.classList.toggle("button-click");
     slider_button_left.classList.toggle("slider-buttons-move");
     slider_button_right.classList.toggle("slider-buttons-move");
 }
+
 function toggle_func(mass, scount, tclass){
     mass[scount].classList.toggle(`${tclass}`);
 }
 
-// function sliderLeftButton(){
-    
-//     for(let slide of slides){
-//         slide_mass.push(slide);
-//     }
-//     for(let small_img of small_imgs){
-//         small_imgs_mass.push(small_img);
-//     }
-//     small_imgs_mass[count].classList.toggle("slider-this-simg"); 
-//     slide_mass[count].classList.toggle("slider-this-bimg");
-//     if(count==0){
-//         count=3
-//         slide_mass[count].classList.toggle("slider-this-bimg");
-        
-//     } else{
-//         count-=1
-//         slide_mass[count].classList.toggle("slider-this-bimg");
-        
-//     }
-//     small_imgs_mass[count].classList.toggle("slider-this-simg");
-// }
-
 function sliderLeftButton(){
     
-    for(let slide of slides){
-        slide_mass.push(slide);
-    }
-    for(let small_img of small_imgs){
-        small_imgs_mass.push(small_img);
-    }
     toggle_func(small_imgs_mass, count, "slider-this-simg");
-    // small_imgs_mass[count].classList.toggle("slider-this-simg"); 
     toggle_func(slide_mass, count, "slider-this-bimg");
     if(count==0){
         count=3
@@ -66,12 +57,6 @@ function sliderLeftButton(){
 
 function sliderRightButton(){
     
-    for(let slide of slides){
-        slide_mass.push(slide);
-    }
-    for(let small_img of small_imgs){
-        small_imgs_mass.push(small_img);
-    }
     toggle_func(small_imgs_mass, count, "slider-this-simg");
     toggle_func(slide_mass, count, "slider-this-bimg");
     if(count==3){
@@ -84,11 +69,32 @@ function sliderRightButton(){
     toggle_func(small_imgs_mass, count, "slider-this-simg");
 }
 
-// function testimonials(){
-//     for(let testi of testimonial){
-//         testi_mass.push(testi);
-//     }
-//     testi_mass[testi_count].classList.toggle("testi-this");
-//     testi_count+=1;
+function smallImgClick(element){
+   
+    for(let i=0; i<small_imgs_mass.length; i++){
+        if(small_imgs_mass[i] == element){
+            toggle_func(small_imgs_mass, count, "slider-this-simg");
+            toggle_func(slide_mass, count, "slider-this-bimg");
+            count = i;
+            toggle_func(small_imgs_mass, count, "slider-this-simg");
+            toggle_func(slide_mass, count, "slider-this-bimg");
+            
+        }
+    }
+    
+}
 
-// }
+function testimonials(element){
+    
+    for(let i=0; i<testi_butt_mass.length; i++){
+        if(testi_butt_mass[i] == element){
+            toggle_func(testi_butt_mass, testi_count, "buttons-this");
+            toggle_func(testi_mass, testi_count, "testi-this");
+            testi_count = i;
+            toggle_func(testi_butt_mass, testi_count, "buttons-this");
+            toggle_func(testi_mass, testi_count, "testi-this");
+            
+        }
+    }
+
+}
