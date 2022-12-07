7d068af348d0c42df2bff945954d1a3021fe98aa
AlekseyMegliy <meglij46@gmail.com>
2022-12-07 13:15:57 +0200

    Make zoom function for home and portfolio pages

--- scripts.js
+++ scripts.js
@@ -3,15 +3,23 @@ let slider_button_left = document.querySelector(".slider-button-left");
 let slider_button_right = document.querySelector(".slider-button-right");
 let slides = document.querySelectorAll(".slide");
 let small_imgs = document.querySelectorAll(".sm-img");
+let home_page_zoom = document.querySelectorAll('.zoom');
+let home_page_zoom_button = document. querySelectorAll(".zoom-button");
+let home_page_remove_zoom = document. querySelectorAll(".remove-zoom");
+let backdrop = document.getElementById("backdrop");
 let testimonial = document.querySelectorAll(".testimonial");
 let testimonial_buttons = document.querySelectorAll(".buttons");
 let benefits = document.querySelectorAll(".benefits div p");
 let slide_mass = [];
 let small_imgs_mass = [];
+let home_page_zoom_mass = [];
+let home_page_zoom_button_mass = [];
+let home_page_remove_zoom_mass = [];
 let testi_mass = [];
 let testi_butt_mass = [];
 let benef_mass = [];
 let count = 0;
+let home_zoom_count =0;
 let testi_count =0;
 let limit = 120;
 
@@ -20,6 +28,15 @@ for(let slide of slides){
 }
 for(let small_img of small_imgs){
     small_imgs_mass.push(small_img);
+}
+for(let zoomer of home_page_zoom){
+    home_page_zoom_mass.push(zoomer);
+}
+for(let zoomer_button of home_page_zoom_button){
+    home_page_zoom_button_mass.push(zoomer_button);
+}
+for(let zoomer_remove of home_page_remove_zoom){
+    home_page_remove_zoom_mass.push(zoomer_remove);
 }
 for(let testi of testimonial){
     testi_mass.push(testi);
@@ -88,6 +105,29 @@ function smallImgClick(element){
         }
     }
     
+}
+
+function zoom(element){
+   
+    for(let i=0; i<home_page_zoom_button_mass.length; i++){
+        if(home_page_zoom_button_mass[i] == element){
+            home_zoom_count = i;
+            toggle_func(home_page_zoom_mass, home_zoom_count, "zoom-this");
+            backdrop.classList.toggle("backdrop")
+            
+        }
+    }
+    
+}
+function removezoom(element){
+    for(let i=0; i<home_page_remove_zoom_mass.length; i++){
+        if(home_page_remove_zoom_mass[i] == element){
+            home_zoom_count = i;
+            toggle_func(home_page_zoom_mass, home_zoom_count, "zoom-this");
+            backdrop.classList.toggle("backdrop")
+            
+        }
+    }
 }
 
 function testimonials(element){
