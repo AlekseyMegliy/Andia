ccfff8547c2cdcd0f8a76d2b948d9c8e62487dea
AlekseyMegliy <meglij46@gmail.com>
2023-02-11 19:23:52 +0200

    formatted

--- scripts.js
+++ scripts.js
@@ -1,217 +1,201 @@
 let header = document.getElementById("header");
-let slider_button_left = document.querySelector(".slider-button-left");
-let slider_button_right = document.querySelector(".slider-button-right");
+let sliderButtonLeft = document.querySelector(".slider-button-left");
+let sliderButtonRight = document.querySelector(".slider-button-right");
 let slides = document.querySelectorAll(".slide");
-let small_imgs = document.querySelectorAll(".sm-img");
-let home_page_zoom = document.querySelectorAll('.zoom');
-let home_page_zoom_button = document.querySelectorAll(".zoom-button");
-let home_page_remove_zoom = document.querySelectorAll(".remove-zoom");
+let smallImgs = document.querySelectorAll(".sm-img");
+let homePageZoom = document.querySelectorAll(".zoom");
+let homePageZoomButton = document.querySelectorAll(".zoom-button");
+let homePageRemoveZoom = document.querySelectorAll(".remove-zoom");
 let backdrop = document.getElementById("backdrop");
-let portfolio_button_all = document.querySelector(".portfolio-button-all");
-let portfolio_button_web = document.querySelector(".portfolio-button-web");
-let portfolio_button_logo = document.querySelector(".portfolio-button-logo");
-let portfolio_button_print = document.querySelector(".portfolio-button-print");
-let filter_web = document.querySelectorAll(".web")
-let filter_logo = document.querySelectorAll(".logo")
-let filter_print = document.querySelectorAll(".print")
+let portfolioButtonAll = document.querySelector(".portfolio-button-all");
+let portfolioButtonWeb = document.querySelector(".portfolio-button-web");
+let portfolioButtonLogo = document.querySelector(".portfolio-button-logo");
+let portfolioButtonPrint = document.querySelector(".portfolio-button-print");
+let filterWeb = document.querySelectorAll(".web");
+let filterLogo = document.querySelectorAll(".logo");
+let filterPrint = document.querySelectorAll(".print");
 let testimonial = document.querySelectorAll(".testimonial");
-let testimonial_buttons = document.querySelectorAll(".buttons");
+let testimonialButtons = document.querySelectorAll(".buttons");
 let benefits = document.querySelectorAll(".benefits div p");
-let slide_mass = [];
-let small_imgs_mass = [];
-let home_page_zoom_mass = [];
-let home_page_zoom_button_mass = [];
-let home_page_remove_zoom_mass = [];
-let testi_mass = [];
-let testi_butt_mass = [];
-let benef_mass = [];
+let slideArr = [];
+let smallImgsArr = [];
+let homePageZoomArr = [];
+let homePageZoomButtonArr = [];
+let homePageRemoveZoomArr = [];
+let testiArr = [];
+let testiButtArr = [];
+let benefArr = [];
 let count = 0;
-let home_zoom_count =0;
-let testi_count =0;
+let homeZoomCount = 0;
+let testiCount = 0;
 let limit = 120;
 
-for(let slide of slides){
-    slide_mass.push(slide);
+for (let slide of slides) {
+  slideArr.push(slide);
 }
-for(let small_img of small_imgs){
-    small_imgs_mass.push(small_img);
+for (let smallImg of smallImgs) {
+  smallImgsArr.push(smallImg);
 }
-for(let zoomer of home_page_zoom){
-    home_page_zoom_mass.push(zoomer);
+for (let zoomer of homePageZoom) {
+  homePageZoomArr.push(zoomer);
 }
-for(let zoomer_button of home_page_zoom_button){
-    home_page_zoom_button_mass.push(zoomer_button);
+for (let zoomerButton of homePageZoomButton) {
+  homePageZoomButtonArr.push(zoomerButton);
 }
-for(let zoomer_remove of home_page_remove_zoom){
-    home_page_remove_zoom_mass.push(zoomer_remove);
+for (let zoomerRemove of homePageRemoveZoom) {
+  homePageRemoveZoomArr.push(zoomerRemove);
 }
-for(let testi of testimonial){
-    testi_mass.push(testi);
+for (let testi of testimonial) {
+  testiArr.push(testi);
 }
-for(let testi_butt of testimonial_buttons){
-    testi_butt_mass.push(testi_butt);
+for (let testiButt of testimonialButtons) {
+  testiButtArr.push(testiButt);
 }
-for(let benefit of benefits){
-    benef_mass.push(benefit);
-    let cont = benefit.innerHTML.split('');
-    let new_cont = cont.slice(0, limit);
-    benefit.innerHTML = new_cont.join('') + " et..."  
-}
-
-
-function headerClick(){
-    header.classList.toggle("button-click");
-    slider_button_left.classList.toggle("slider-buttons-move");
-    slider_button_right.classList.toggle("slider-buttons-move");
+for (let benefit of benefits) {
+  benefArr.push(benefit);
+  let cont = benefit.innerHTML.split("");
+  let newCont = cont.slice(0, limit);
+  benefit.innerHTML = newCont.join("") + " et...";
 }
 
-function toggle_func(mass, scount, tclass){
-    mass[scount].classList.toggle(`${tclass}`);
+function headerClick() {
+  header.classList.toggle("button-click");
+  sliderButtonLeft.classList.toggle("slider-buttons-move");
+  sliderButtonRight.classList.toggle("slider-buttons-move");
 }
 
-function sliderLeftButton(){
-    
-    toggle_func(small_imgs_mass, count, "slider-this-simg");
-    toggle_func(slide_mass, count, "slider-this-bimg");
-    if(count==0){
-        count=3
-        toggle_func(slide_mass, count, "slider-this-bimg");
-        
-    } else{
-        count-=1
-        toggle_func(slide_mass, count, "slider-this-bimg");
-        
+function toggleFunc(mass, scount, tclass) {
+  mass[scount].classList.toggle(`${tclass}`);
+}
+
+function sliderLeftButton() {
+  toggleFunc(smallImgsArr, count, "slider-this-simg");
+  toggleFunc(slideArr, count, "slider-this-bimg");
+  if (count == 0) {
+    count = 3;
+    toggleFunc(slideArr, count, "slider-this-bimg");
+  } else {
+    count -= 1;
+    toggleFunc(slideArr, count, "slider-this-bimg");
+  }
+  toggleFunc(smallImgsArr, count, "slider-this-simg");
+}
+
+function sliderRightButton() {
+  toggleFunc(smallImgsArr, count, "slider-this-simg");
+  toggleFunc(slideArr, count, "slider-this-bimg");
+  if (count == 3) {
+    count = 0;
+    toggleFunc(slideArr, count, "slider-this-bimg");
+  } else {
+    count += 1;
+    toggleFunc(slideArr, count, "slider-this-bimg");
+  }
+  toggleFunc(smallImgsArr, count, "slider-this-simg");
+}
+
+function smallImgClick(element) {
+  for (let i = 0; i < smallImgsArr.length; i++) {
+    if (smallImgsArr[i] == element) {
+      toggleFunc(smallImgsArr, count, "slider-this-simg");
+      toggleFunc(slideArr, count, "slider-this-bimg");
+      count = i;
+      toggleFunc(smallImgsArr, count, "slider-this-simg");
+      toggleFunc(slideArr, count, "slider-this-bimg");
     }
-    toggle_func(small_imgs_mass, count, "slider-this-simg");
+  }
 }
 
-function sliderRightButton(){
-    
-    toggle_func(small_imgs_mass, count, "slider-this-simg");
-    toggle_func(slide_mass, count, "slider-this-bimg");
-    if(count==3){
-        count=0
-        toggle_func(slide_mass, count, "slider-this-bimg");
-    } else{
-        count+=1
-        toggle_func(slide_mass, count, "slider-this-bimg");
+function zoom(element) {
+  for (let i = 0; i < homePageZoomButtonArr.length; i++) {
+    if (homePageZoomButtonArr[i] == element) {
+      homeZoomCount = i;
+      toggleFunc(homePageZoomArr, homeZoomCount, "zoom-this");
+      backdrop.classList.toggle("backdrop");
     }
-    toggle_func(small_imgs_mass, count, "slider-this-simg");
+  }
+}
+function removezoom(element) {
+  for (let i = 0; i < homePageRemoveZoomArr.length; i++) {
+    if (homePageRemoveZoomArr[i] == element) {
+      homeZoomCount = i;
+      toggleFunc(homePageZoomArr, homeZoomCount, "zoom-this");
+      backdrop.classList.toggle("backdrop");
+    }
+  }
 }
 
-function smallImgClick(element){
-   
-    for(let i=0; i<small_imgs_mass.length; i++){
-        if(small_imgs_mass[i] == element){
-            toggle_func(small_imgs_mass, count, "slider-this-simg");
-            toggle_func(slide_mass, count, "slider-this-bimg");
-            count = i;
-            toggle_func(small_imgs_mass, count, "slider-this-simg");
-            toggle_func(slide_mass, count, "slider-this-bimg");
-            
-        }
+function filter(type) {
+  if (type == "all") {
+    portfolioButtonAll.classList.add("portfolio-button-this");
+    portfolioButtonWeb.classList.remove("portfolio-button-this");
+    portfolioButtonLogo.classList.remove("portfolio-button-this");
+    portfolioButtonPrint.classList.remove("portfolio-button-this");
+    for (web of filterWeb) {
+      web.classList.remove("inviz");
     }
-    
+    for (logo of filterLogo) {
+      logo.classList.remove("inviz");
+    }
+    for (prints of filterPrint) {
+      prints.classList.remove("inviz");
+    }
+  }
+  if (type == "web") {
+    portfolioButtonAll.classList.remove("portfolio-button-this");
+    portfolioButtonWeb.classList.add("portfolio-button-this");
+    portfolioButtonLogo.classList.remove("portfolio-button-this");
+    portfolioButtonPrint.classList.remove("portfolio-button-this");
+    for (web of filterWeb) {
+      web.classList.remove("inviz");
+    }
+    for (logo of filterLogo) {
+      logo.classList.add("inviz");
+    }
+    for (prints of filterPrint) {
+      prints.classList.add("inviz");
+    }
+  }
+  if (type == "logo") {
+    portfolioButtonAll.classList.remove("portfolio-button-this");
+    portfolioButtonWeb.classList.remove("portfolio-button-this");
+    portfolioButtonLogo.classList.add("portfolio-button-this");
+    portfolioButtonPrint.classList.remove("portfolio-button-this");
+    for (web of filterWeb) {
+      web.classList.add("inviz");
+    }
+    for (logo of filterLogo) {
+      logo.classList.remove("inviz");
+    }
+    for (prints of filterPrint) {
+      prints.classList.add("inviz");
+    }
+  }
+  if (type == "print") {
+    portfolioButtonAll.classList.remove("portfolio-button-this");
+    portfolioButtonWeb.classList.remove("portfolio-button-this");
+    portfolioButtonLogo.classList.remove("portfolio-button-this");
+    portfolioButtonPrint.classList.add("portfolio-button-this");
+    for (web of filterWeb) {
+      web.classList.add("inviz");
+    }
+    for (logo of filterLogo) {
+      logo.classList.add("inviz");
+    }
+    for (prints of filterPrint) {
+      prints.classList.remove("inviz");
+    }
+  }
 }
 
-function zoom(element){
-   
-    for(let i=0; i<home_page_zoom_button_mass.length; i++){
-        if(home_page_zoom_button_mass[i] == element){
-            home_zoom_count = i;
-            toggle_func(home_page_zoom_mass, home_zoom_count, "zoom-this");
-            backdrop.classList.toggle("backdrop")
-            
-        }
+function testimonials(element) {
+  for (let i = 0; i < testiButtArr.length; i++) {
+    if (testiButtArr[i] == element) {
+      toggleFunc(testiButtArr, testiCount, "buttons-this");
+      toggleFunc(testiArr, testiCount, "testi-this");
+      testiCount = i;
+      toggleFunc(testiButtArr, testiCount, "buttons-this");
+      toggleFunc(testiArr, testiCount, "testi-this");
     }
-    
+  }
 }
-function removezoom(element){
-    for(let i=0; i<home_page_remove_zoom_mass.length; i++){
-        if(home_page_remove_zoom_mass[i] == element){
-            home_zoom_count = i;
-            toggle_func(home_page_zoom_mass, home_zoom_count, "zoom-this");
-            backdrop.classList.toggle("backdrop")
-            
-        }
-    }
-}
-
-function filter(type){
-    if(type == 'all'){
-        portfolio_button_all.classList.add("portfolio-button-this");
-        portfolio_button_web.classList.remove("portfolio-button-this");
-        portfolio_button_logo.classList.remove("portfolio-button-this");
-        portfolio_button_print.classList.remove("portfolio-button-this");
-        for( web of filter_web){
-            web.classList.remove("inviz")    
-        }
-        for( logo of filter_logo){
-            logo.classList.remove("inviz")
-        }
-        for( prints of filter_print){
-            prints.classList.remove("inviz")
-        }
-    }
-    if(type == 'web'){
-        portfolio_button_all.classList.remove("portfolio-button-this");
-        portfolio_button_web.classList.add("portfolio-button-this");
-        portfolio_button_logo.classList.remove("portfolio-button-this");
-        portfolio_button_print.classList.remove("portfolio-button-this");
-        for( web of filter_web){
-            web.classList.remove("inviz")
-        }
-        for( logo of filter_logo){
-            logo.classList.add("inviz")
-        }
-        for( prints of filter_print){
-            prints.classList.add("inviz")
-        }
-    }
-    if(type == 'logo'){
-        portfolio_button_all.classList.remove("portfolio-button-this");
-        portfolio_button_web.classList.remove("portfolio-button-this");
-        portfolio_button_logo.classList.add("portfolio-button-this");
-        portfolio_button_print.classList.remove("portfolio-button-this");
-        for( web of filter_web){
-            web.classList.add("inviz")
-        }
-        for( logo of filter_logo){
-            logo.classList.remove("inviz")
-        }
-        for( prints of filter_print){
-            prints.classList.add("inviz")
-        }
-    }
-    if(type == 'print'){
-        portfolio_button_all.classList.remove("portfolio-button-this");
-        portfolio_button_web.classList.remove("portfolio-button-this");
-        portfolio_button_logo.classList.remove("portfolio-button-this");
-        portfolio_button_print.classList.add("portfolio-button-this");
-        for( web of filter_web){
-            web.classList.add("inviz")
-        }
-        for( logo of filter_logo){
-            logo.classList.add("inviz")
-        }
-        for( prints of filter_print){
-            prints.classList.remove("inviz")
-        }
-    }
-}
-
-function testimonials(element){
-    
-    for(let i=0; i<testi_butt_mass.length; i++){
-        if(testi_butt_mass[i] == element){
-            toggle_func(testi_butt_mass, testi_count, "buttons-this");
-            toggle_func(testi_mass, testi_count, "testi-this");
-            testi_count = i;
-            toggle_func(testi_butt_mass, testi_count, "buttons-this");
-            toggle_func(testi_mass, testi_count, "testi-this");
-            
-        }
-    }
-
-}
-
